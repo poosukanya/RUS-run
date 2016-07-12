6ac7e6e37fe76a945c685ca8e36c175f3e43758d
sukanya <kampoo2416@gmail.com>
2016-07-12 16:35:02 +0700

    finish 12 july

--- app/src/main/java/rus/saramon/sukanya/rusrun/SignUpActivity.java
+++ app/src/main/java/rus/saramon/sukanya/rusrun/SignUpActivity.java
@@ -20,6 +20,7 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+
         //Bind Widget
         nameEditText = (EditText) findViewById(R.id.editText);
         userEditText = (EditText) findViewById(R.id.editText2);
