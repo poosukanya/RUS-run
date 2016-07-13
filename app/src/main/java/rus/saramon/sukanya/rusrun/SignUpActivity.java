6c734d9cad79d69814b6d93caf13331755b1dfb6
sukanya <kampoo2416@gmail.com>
2016-07-13 14:16:10 +0700

    Update finish

--- app/src/main/java/rus/saramon/sukanya/rusrun/SignUpActivity.java
+++ app/src/main/java/rus/saramon/sukanya/rusrun/SignUpActivity.java
@@ -7,6 +7,16 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignUpActivity extends AppCompatActivity {
 
     //Explicit
@@ -15,6 +25,7 @@ public class SignUpActivity extends AppCompatActivity {
     private RadioButton avata0RadioButton, avata1RadioButton,
             avata2RadioButton, avata3RadioButton, avata4RadioButton;
     private  String nameString, userString, passwordString, avataString;
+    private static final String urlPHP = "http://swiftcodingthai.com/rus/add_user_master.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +88,7 @@ public class SignUpActivity extends AppCompatActivity {
 
         } else if (checkChoose()) {
             //Checked
+            updateNewUserToSever();
 
         } else {
             //Un check
@@ -87,6 +99,32 @@ public class SignUpActivity extends AppCompatActivity {
 
 
     }   //clickSignUp
+
+    private void updateNewUserToSever() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .add("Avata", avataString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
+    }   // update
 
     private boolean checkChoose() {
         boolean status = true;
